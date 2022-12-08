3005171c63b9907097f8be8eefdf7e33a4d4fa2c
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-08 18:43:03 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 
 module.exports = {
-  token: process.env.TOKEN || '', // your discord bot token
+  token: process.env.TOKEN || 'MTA0MDkzNzc1NTM0NzI1OTQ1Mw.GkQ_Z1.a6EkLriWdgSLtZtmd3hGRZOeJYxZXNYo1RamSg', // your discord bot token
   prefix: process.env.PREFIX || '.', // bot prefix
-  ownerID: process.env.OWNERID || ['959276033683628122', '952560202635427841'], //your discord id
+  ownerID: process.env.OWNERID || ['1040933465463468103'], //your discord id
   SpotifyID: process.env.SPOTIFYID || '', // spotify client id
   SpotifySecret: process.env.SPOTIFYSECRET || '', // spotify client secret
-  mongourl: process.env.MONGO_URI || 'mongodb+srv://', // MongoDb URL
+  mongourl: process.env.MONGO_URI || 'mongodb+srv://Mark:Katasandi@cluster0.ev4r4x6.mongodb.net/?retryWrites=true&w=majority', // MongoDb URL
   embedColor: process.env.COlOR || '#3366ff', // embed colour
-  logs: process.env.LOGS || '', // Discord channel id 
+  logs: process.env.LOGS || '1040933814710566982', // Discord channel id 
   links: {
-    support: process.env.SUPPORT || 'https://discord.gg/ns8CTk9J3e',
-    invite: process.env.INVITE || 'https://discord.gg/ns8CTk9J3e',
-    vote: process.env.VOTE || 'https://discord.gg/ns8CTk9J3e',
+    support: process.env.SUPPORT || 'https://discord.gg/eufj5nqw',
+    invite: process.env.INVITE || 'https://discord.gg/eufj5nqw',
+    vote: process.env.VOTE || 'https://discord.gg/eufj5nqw',
     bg: process.env.BG || 'https://media.discordapp.net/attachments/966675680907657256/967789748699668480/flat-landscape-lake-sunset-deer-wallpaper-preview.jpg'
   },
 
